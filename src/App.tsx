cea3c28a24eb28bfc0b18d8c82db2f77bce8e219
Incredible Git Expert <85891155+CheaterTim@users.noreply.github.com>
2023-05-04 17:04:33 +0200

    no more

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
 
   useEffect(() => {
     (async()=>{
-      const response = await fetch("/data.json")
+      const response = await fetch("data.json")
       setData(await response.json())
     })();
   }, []);
